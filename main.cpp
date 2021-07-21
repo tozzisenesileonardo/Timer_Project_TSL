560fe2d4bc40bd6b8aba657fac85b6b847f2d523
tozzisenesileonardo <78380125+tozzisenesileonardo@users.noreply.github.com>
2021-07-21 10:50:21 +0200

    Add files via upload
    Prima modifica

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <wx/wx.h>
-
+#include <iostream>
+using namespace std;
 class Simple : public wxFrame{
 public:
     Simple(const wxString& title)
@@ -12,10 +13,12 @@ public:
 
 class MyApp : public wxApp{
     bool OnInit(){
+        cout<<"Hello World";
         Simple *simple = new Simple(wxT("simple"));
         simple->Show(true);
         return true;
     }
 };
+
 
 wxIMPLEMENT_APP(MyApp);
